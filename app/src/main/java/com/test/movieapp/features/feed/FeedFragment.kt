b3e5ef1749c6ae00ea6ffb13ba70b9ccfa569496
SsongSik <sgsk88@nate.com>
2023-07-02 23:52:17 +0900

    [Nav_Graph] ComposeStudy -> MovieAppCompose Repo Change, NavGraph Add

---
+++ app/src/main/java/com/test/movieapp/features/feed/FeedFragment.kt
@@ -0,0 +1,27 @@
+package com.test.movieapp.features.feed
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.test.movieapp.ui.theme.MovieAppTheme
+
+class FeedFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireActivity()).apply {
+            setContent {
+                MovieAppTheme {
+                    Text("FeedFragment")
+                }
+            }
+        }
+    }
+}
